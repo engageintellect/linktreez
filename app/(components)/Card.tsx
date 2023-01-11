1741fbf420f737274e6e7a1cc534f846d5910463
engageintellect <jc9361@gmail.com>
2023-01-10 23:06:46 -0800

    changing colors

--- app/(components)/Card.tsx
+++ app/(components)/Card.tsx
@@ -16,9 +16,9 @@ export default function Card({
       <a
         href={url}
         target="_blank"
-        className="my-2 group transition-color duration-300 hover:bg-gray-900 bg-gray-800 h-20 w-full sm:w-[66vw] md:w-[50vw] xl:w-[33vw] rounded-lg shadow-lg flex items-center justify-between"
+        className="my-2 group transition-color duration-300 hover:bg-zinc-900 bg-zinc-800 h-20 w-full sm:w-[66vw] md:w-[50vw] xl:w-[33vw] rounded-lg shadow-lg flex items-center justify-between"
       >
-        <div className="flex items-center group-hover:shadow-lg justify-center h-16 bg-gray-800 w-20 m-2 rounded-lg">
+        <div className="flex items-center group-hover:shadow-lg justify-center h-16 bg-zinc-800 w-20 m-2 rounded-lg">
           <div className="text-4xl md:group-hover:scale-[120%] transition-transform duration-500">
             {icon}
           </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <head />
       <body>
         {/* <Navbar /> */}
-        <div className="min-h-screen bg-gradient-to-t from-gray-800 to-gray-900 p-2">
+        <div className="min-h-screen bg-gradient-to-t from-zinc-800 to-zinc-900 p-2">
           {children}
         </div>
         <Footer />
