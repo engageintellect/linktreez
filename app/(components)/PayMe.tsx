2d1a6f8e24691a936d7f1a2d9480b2e631e39d1e
engage_intelllect <61082194+engageintellect@users.noreply.github.com>
2023-03-18 12:10:11 -0700

    making links open in new window

--- app/(components)/PayMe.tsx
+++ app/(components)/PayMe.tsx
@@ -7,6 +7,7 @@ export default function PayMe() {
       <div className="h-16 w-full flex items text-xl font-semibold mb-2 gap-2 sm:w-[66vw] md:w-[50vw] xl:w-[33vw]">
         <a
           href={items.cashapp.url}
+          target="_blank"
           className="group text-4xl w-full bg-green-700 hover:bg-green-800 rounded-lg flex items-center justify-center transition-colors duration-300"
         >
           <div className="group-hover:scale-[110%] transition-transform duration-500">
@@ -15,6 +16,7 @@ export default function PayMe() {
         </a>
         <a
           href={items.bitcoin.url}
+          target="_blank"
           className="group text-4xl w-full bg-amber-700 hover:bg-amber-800 rounded-lg flex items-center justify-center transition-colors duration-300"
         >
           <div className="group-hover:scale-[110%] transition-transform duration-500">
