19f42c8b34106dee2f76196a91dc1343078e1778
engage_intelllect <61082194+engageintellect@users.noreply.github.com>
2023-03-18 12:06:38 -0700

    adding btc address

--- data/data.ts
+++ data/data.ts
@@ -7,7 +7,7 @@ export const config = {
 export const items = {
   bitcoin: {
     title: "Bitcoin",
-    url: "https://www.google.com/search?q=youtube+rick+roll&rlz=1C1YTUH_enUS1029US1029&oq=youtube+rick+roll&aqs=chrome..69i57j69i64.2546j0j7&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:0c2941ce,vid:eBGIQ7ZuuiU",
+    url: "https://mempool.space/address/bc1qeywstujt6lkvfcwr30kklavr25jxvr7d53jh0j",
   },
 
   cashapp: {
