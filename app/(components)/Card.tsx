137562d53b3527426bd95d624143b492834c715a
engageintellect <jc9361@gmail.com>
2023-03-18 23:43:30 -0700

    init

--- app/(components)/Card.tsx
+++ app/(components)/Card.tsx
@@ -16,16 +16,16 @@ export default function Card({
       <a
         href={url}
         target="_blank"
-        className="my-2 group transition-color duration-300 focus:bg-zinc-900 sm:hover:bg-zinc-900 bg-zinc-800 h-20 w-full sm:w-[66vw] md:w-[50vw] xl:w-[33vw] rounded-lg shadow-lg flex items-center justify-between"
+        className="my-2 group transition-color duration-300 hover:bg-zinc-900 bg-zinc-800 h-20 w-full sm:w-[66vw] md:w-[50vw] xl:w-[33vw] rounded-lg shadow-lg flex items-center justify-between"
         rel="noreferrer"
       >
-        <div className="flex items-center sm:group-hover:shadow-lg justify-center h-16 bg-zinc-800 w-20 m-2 rounded-lg">
+        <div className="flex items-center group-hover:shadow-lg justify-center h-16 bg-zinc-800 w-20 m-2 rounded-lg">
           <div className="text-4xl md:group-hover:scale-[120%] transition-transform duration-500">
             {icon}
           </div>
         </div>
 
-        <div className="px-10 text-xl sm:group-hover:font-semibold md:group-hover:-translate-x-5 transition-transform duration-300">
+        <div className="px-10 text-xl group-hover:font-semibold md:group-hover:-translate-x-5 transition-transform duration-300">
           {title}
         </div>
       </a>
