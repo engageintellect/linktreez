1beeffdc693cfffad6f8905c0ca2d0adef577ed7
engageintellect <jc9361@gmail.com>
2023-03-26 00:46:09 -0700

    cleaning up

--- app/(components)/PayMe.tsx
+++ app/(components)/PayMe.tsx
@@ -4,11 +4,12 @@ import { items } from "../../data/data";
 export default function PayMe() {
   return (
     <>
-      <div className="h-16 w-full flex items text-xl font-semibold mb-2 gap-2 sm:w-[66vw] md:w-[50vw] xl:w-[33vw]">
+      <div className="h-16 w-full flex items text-xl font-semibold mb-2 gap-2">
         <a
           href={items.cashapp.url}
           target="_blank"
           className="group text-4xl w-full bg-green-700 hover:bg-green-800 rounded-lg flex items-center justify-center transition-colors duration-300"
+          rel="noreferrer"
         >
           <div className="group-hover:scale-[110%] transition-transform duration-500">
             <FaDollarSign />
@@ -18,6 +19,7 @@ export default function PayMe() {
           href={items.bitcoin.url}
           target="_blank"
           className="group text-4xl w-full bg-amber-700 hover:bg-amber-800 rounded-lg flex items-center justify-center transition-colors duration-300"
+          rel="noreferrer"
         >
           <div className="group-hover:scale-[110%] transition-transform duration-500">
             <FaBtc />
--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ import { items, tagline } from "../data/data";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center w-full max-w-xl mx-auto">
         <section className="flex flex-col items-center gap-5 justify-center my-10">
           <Avatar />
           <div className="text-2xl font-semibold">
