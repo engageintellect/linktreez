72174bbb04e23aa9fdd03f18821935af0f8dd052
engageintellect <jc9361@gmail.com>
2023-05-16 21:57:54 -0700

    cleaning

--- app/(components)/Card.tsx
+++ app/(components)/Card.tsx
@@ -19,12 +19,12 @@ export default function Card({
         className="my-2 group transition-color bg-zinc-800/50 duration-300 h-20 w-full rounded-lg shadow-lg flex items-center justify-between"
         rel="noreferrer"
       >
-        <div className="px-10 text-l font-bold transition-transform duration-300">
+        <div className="px-5 md:px-10 text-l font-bold transition-transform duration-300 md:group-hover:scale-110">
           {title}
         </div>
 
-        <div className="flex items-center group-hover:shadow-lg transition-all duration-200 md:group-hover:bg-zinc-700/50 justify-center h-16 w-20 m-2 rounded-lg mask mask-squircle">
-          <div className="text-4xl md:group-hover:scale-[120%] transition-transform duration-500 px-10">
+        <div className="flex items-center group-hover:shadow-lg transition-all duration-200 md:group-hover:bg-zinc-700/50 justify-center h-16 w-20 m-2 rounded-lg mask mask-squircle px-5 md:px-10">
+          <div className="text-4xl md:group-hover:scale-[120%] transition-transform duration-500">
             {icon}
           </div>
         </div>
