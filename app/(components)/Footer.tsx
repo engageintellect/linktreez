1b655fb41f7488a651bfb7a57c4e5eb7636738d4
engageintellect <jc9361@gmail.com>
2023-08-20 00:10:57 -0700

    cleaning up styles

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="py-2 text-center bg-zinc-800">
       <a href={config.url}>
-        <div className="flex justify-center items-center gap-2">
+        <div className="flex justify-center items-center gap-2 py-2">
           <div>{config.footer}</div>
           <div>
             <FaGithub />
